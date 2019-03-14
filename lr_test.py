ee15a14d00b3b23318e9210a372b69a741cc9d80
Deepak <deepak.rajendran123@gmail.com>
2019-03-14 16:17:42 +0530

    Completed tf-idf matrix making

--- lr_test.py
+++ lr_test.py
@@ -1,37 +1,58 @@
 import pandas as pd
+import numpy as np
 import gensim.downloader as api
 from gensim.models import TfidfModel
 from gensim.corpora import Dictionary
 import csv
 from os import path,mkdir,fsync
 
-# with open("data.csv", "r") as f:
-#     reader = csv.reader(f, delimiter=",")
-#     for line in enumerate(reader):
-#         listWord=[]
-#         listWord.append(line[7].split())
-# print(listWord)
+def zero(column,row): 
+    return np.zeros([column,row])
 
-# dct = Dictionary(listWord)
+def getMatrix(corpus,model):
+    i = 0
+    dataDict = {}
+    maxDocLength = 0
+    for doc in corpus:
+        print('Doing doc ' + str(i),end = '   ')
+        index = 0
+        counter = 1
+        rowArray = np.array([])
+        while True:
+            try:
+                word = model[doc][index]
+                if(word[0]==counter):
+                    rowArray = np.append(rowArray,word[1])
+                    counter = counter + 1
+                    index += 1
+                else:
+                    rowArray = np.append(rowArray,np.float64(0))
+                    counter = counter + 1
+            except:
+                break
+        docName = 'doc' + str(i)
+        i += 1 
+        maxDocLength = max(rowArray.size,maxDocLength)
+        print(maxDocLength)
+        dataDict[docName] = rowArray
+        if(i%5 == 0 and i!= 0):
+            break
+    for key,item in dataDict.items():
+        dataDict[key] = np.append(item,np.zeros(maxDocLength - item.size))
+        assert(dataDict[key].size == maxDocLength)
+    df = pd.DataFrame(dataDict)
+    df.to_csv('wordMatrix.csv',index=False)
 
-dataset = api.load("text8")
-dct = Dictionary(dataset)
 
-corpus = [dct.doc2bow(line) for line in dataset] 
-model = TfidfModel(corpus)  
-vector = {}
+def main():
+    dataset = api.load("text8")
+    dct = Dictionary(dataset)
 
-if(path.exists('./lr.csv')):
-        aliasFile = open('lr.csv','a')
-else:
-    aliasFile = open('lr.csv','a')
-    #aliasFile.write("slno,hostname\n")
+    corpus = [dct.doc2bow(line) for line in dataset] 
+    model = TfidfModel(corpus)  
 
-for i in range(100):
-    vector[i]=model[corpus[i]]
-    finalFile.write(vector[i])
-    
-print(vector)
+    getMatrix(corpus,model)
 
-    
 
+if __name__ == "__main__":
+    main()
